345a5176a32c7d1747fb596933d6d93e5dc9b330
MostafaM.7 <38378540+samangostarts@users.noreply.github.com>
2025-06-11 16:04:52 +0330

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TOKEN = os.getenv("7245345277:AAG6u2O8KRWa_mc2nUBD1vzAgMUirEm0Fz4")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات فعال شد 😊")
